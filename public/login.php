793a999c95a034a6e9256b0f9be2e00305943ba1
kalinotto <kalin.otto@gmail.com>
2017-12-04 09:57:57 -0500

    latest version
    fixed some stuff

--- public/login.php
+++ public/login.php
@@ -1,23 +1,21 @@
 <?php
+session_start();
 require 'db.php';
 
-if(isset($_GET['uname']) && isset($_GET['psw'])) {
-	$uname = $_GET['uname'];
-	$phash = $_GET['psw'];
+$uname = $_POST['uname'];
+$phash = $_POST['psw'];
 
-	$sql = "SELECT name FROM Users WHERE name = '$uname' AND phash = '$phash'";
-	echo "$sql";
-	$db = getDB();
+$sql = "SELECT name FROM Users WHERE name = '$uname' AND phash = '$phash'";
+$db = getDB();
 
-	$stmt = $db->query($sql);
-	if ($stmt->rowCount() != 1) {
-		// login failed
-		echo "Login failed.";
-	} else {
-		$row = $stmt->fetch();
-		$_SESSION['username'] = $row->name;
-		$_SESSION['success'] = "Successful login.";
-		header('location: index.html');
-	}
+$stmt = $db->query($sql);
+if ($stmt->rowCount() != 1) {
+	// login failed
+	echo "Login failed. <a href='index.html'>Return</a>";
+} else {
+	$row = $stmt->fetch();
+	$_SESSION['username'] = $row->name;
+	$_SESSION['success'] = true;
+	header('location: index.html');
 }
 ?>
